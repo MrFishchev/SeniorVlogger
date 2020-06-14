bd5ed1904b34c4fc694c8d643c2e56f83cc638e3
mrfishchev <dopingoo@gmail.com>
2020-06-14 12:55:04 +0300

    Added AuthorId to BlogPost model

---
+++ SeniorVlogger.DataAccess/Migrations/20200614092048_AddedAuthorIdToBlogPostModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeniorVlogger.DataAccess.Migrations
+{
+    public partial class AddedAuthorIdToBlogPostModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorId",
+                table: "BlogPosts",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPosts_AuthorId",
+                table: "BlogPosts",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BlogPosts_AspNetUsers_AuthorId",
+                table: "BlogPosts",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BlogPosts_AspNetUsers_AuthorId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BlogPosts_AuthorId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "BlogPosts");
+        }
+    }
+}
