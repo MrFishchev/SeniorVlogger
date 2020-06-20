8a7c92f8e89152e9116452068d55dda4feecc043
mrfishchev <dopingoo@gmail.com>
2020-06-20 15:08:59 +0300

    Saving blog post to DB

--- SeniorVlogger.Web/Controllers/BlogController.cs
+++ SeniorVlogger.Web/Controllers/BlogController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SeniorVlogger.DataAccess.Repository.IRepository;
+using SeniorVlogger.Models;
 using SeniorVlogger.Models.DTO;
+using SeniorVlogger.Web.Extensions;
+using SeniorVlogger.Web.Services;
+using SlugGenerator;
 
 namespace SeniorVlogger.Web.Controllers
 {
@@ -18,21 +23,52 @@ namespace SeniorVlogger.Web.Controllers
     {
         private readonly ILogger<BlogController> _logger;
         private readonly IUnitOfWork _unitOfWork;
-        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly UploadsService _uploadsService;
 
-        public BlogController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment, ILogger<BlogController> logger)
+        public BlogController(IUnitOfWork unitOfWork, UploadsService uploadsService, ILogger<BlogController> logger)
         {
             _unitOfWork = unitOfWork;
-            _hostEnvironment = hostEnvironment;
+            _uploadsService = uploadsService;
             _logger = logger;
         }
 
-        [HttpGet]
-        public async Task<IEnumerable<BlogPost>> GetAll()
+        [HttpPost("image")]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> SaveImage([FromForm] IFormCollection form)
         {
-            var allObj = await _unitOfWork.BlogPosts
-                .GetAll(includeProperties: "Next,Previous");
-            return allObj;
+            if (!form.Files.Any()) return BadRequest();
+
+            var file = form.Files.First();
+            var result = await _uploadsService.Upload(file);
+
+            if (string.IsNullOrEmpty(result)) return BadRequest();
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SavePost([FromBody] BlogPost post)
+        {
+            try
+            {
+                var email = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var user = await _unitOfWork.ApplicationUsers
+                    .GetFirstOrDefault(u => u.Email == email);
+
+                var objDb = post.ToDto();
+                objDb.AuthorId = user.Id;
+                objDb.Slug = post.Title.GenerateSlug();
+                //TODO Generate unique slug by DB
+
+                await _unitOfWork.BlogPosts.Add(objDb);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Cannot create blog post: {e.Message}");
+                return Problem();
+            }
+
+            return Ok();
         }
 
         [HttpDelete]
---
+++ SeniorVlogger.Web/Extensions/BlogPostExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Internal;
+using SeniorVlogger.Models;
+using SeniorVlogger.Models.DTO;
+
+namespace SeniorVlogger.Web.Extensions
+{
+    public static class BlogPostExtension
+    {
+        public static BlogPostDto ToDto(this BlogPost post)
+        {
+            return new BlogPostDto
+            {
+                CategoryId = post.Category.Id,
+                NextId = post?.Next?.Id,
+                PreviousId = post?.Previous?.Id,
+                Title = post.Title,
+                ImageUrl = post.ImageUrl,
+                Content = post.Content,
+                Description = post.Description,
+                Mailed = post.Mailed,
+                Scratch = post.Scratch,
+                PublishDate = post.PublishDate,
+                Tags = string.Join(',', post.Tags)
+            };
+        }
+    }
+}
