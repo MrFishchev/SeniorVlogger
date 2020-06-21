028cb93e6ff73b0c1e9871bce9c9757d090ef1e6
mrfishchev <dopingoo@gmail.com>
2020-06-21 17:33:55 +0300

    Added sweetalerts2; Handling 401 error

--- SeniorVlogger.Web/ClientApp/src/main.js
+++ SeniorVlogger.Web/ClientApp/src/main.js
@@ -13,6 +13,10 @@ Vue.use(VueGoodTablePlugin);
 import Notifications from 'vue-notification'
 Vue.use(Notifications)
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css'
+Vue.use(VueSweetalert2);
+
 import Default from "./layouts/Default.vue"
 import About from "./layouts/About.vue"
 import Blog from "./layouts/Blog.vue"
@@ -26,15 +30,25 @@ Vue.component('mainslide-layout', MainSlide)
 Vue.component('manage-layout', Manage)
 
 axios.interceptors.request.use(config => {
-    let token = store.getters.TOKEN
-    if (token) {
-        config.headers.Authorization = 'Bearer ' + token
-    }
-
-    return config
-}, (error) => {
+        let token = store.getters.TOKEN
+        if (token) {
+            config.headers.Authorization = 'Bearer ' + token
+        }
+        return config
+    }, 
+    (error) => {
     return Promise.reject(error)
 })
+
+axios.interceptors.response.use(response => { return response }, error => {
+    if(error.response.status === 401){
+        store.commit('DELETE_USER')
+        router.replace({ path: '/login'})
+    }
+    return Promise.reject(error)
+})
+
+
 Vue.prototype.$api = axios
 
 Vue.config.productionTip = false
