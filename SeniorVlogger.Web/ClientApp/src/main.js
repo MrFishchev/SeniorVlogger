78b7418e6e04d19a4e73f5bcd0b576e587ce9281
mrfishchev <dopingoo@gmail.com>
2020-06-27 13:47:27 +0300

    Added global loader for axios calls

--- SeniorVlogger.Web/ClientApp/src/main.js
+++ SeniorVlogger.Web/ClientApp/src/main.js
@@ -6,14 +6,18 @@ import router from './router'
 import './assets/bootstrap.min.css'
 import './assets/style.sass'
 
-import VueGoodTablePlugin from 'vue-good-table';
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+Vue.use(Loading);
+
+import VueGoodTablePlugin from 'vue-good-table'
 import 'vue-good-table/dist/vue-good-table.css'
 Vue.use(VueGoodTablePlugin);
 
 import Notifications from 'vue-notification'
 Vue.use(Notifications)
 
-import VueSweetalert2 from 'vue-sweetalert2';
+import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.css'
 Vue.use(VueSweetalert2);
 
@@ -29,7 +33,17 @@ Vue.component('blog-layout', Blog)
 Vue.component('mainslide-layout', MainSlide)
 Vue.component('manage-layout', Manage)
 
+let loader 
 axios.interceptors.request.use(config => {
+            if(loader && loader.isActive) return
+            loader = Vue.$loading.show({
+                color: '#079af0',
+                loader: 'bars',
+                width: 128,
+                height: 128,
+                opacity: 0.5
+            })
+  
         let token = store.getters.TOKEN
         if (token) {
             config.headers.Authorization = 'Bearer ' + token
@@ -40,7 +54,11 @@ axios.interceptors.request.use(config => {
     return Promise.reject(error)
 })
 
-axios.interceptors.response.use(response => { return response }, error => {
+axios.interceptors.response.use(response => { 
+    if(loader && loader.isActive) loader.hide()
+    return response 
+}, error => {
+    if(loader && loader.isActive) loader.hide()
     if(error.response.status === 401){
         store.commit('DELETE_USER')
         router.replace({ path: '/login'})
